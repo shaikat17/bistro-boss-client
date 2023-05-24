709c3d4117808f6c56c545fd74ece84689726eb2
shaikat17 <shaikatpal56@gmail.com>
2023-05-24 12:36:39 +0600

    footer section done

--- src/components/CommonLayout.jsx
+++ src/components/CommonLayout.jsx
@@ -1,10 +1,14 @@
+import { Outlet } from "react-router-dom";
 import Navbar from "./shared/Navbar";
+import Footer from "./shared/Footer";
 
 
 const CommonLayout = () => {
     return (
         <>
             <Navbar />
+            <Outlet />
+            <Footer />
         </>
     );
 };
